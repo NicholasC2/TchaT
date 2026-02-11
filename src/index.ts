fcd86e41126fc5554302ea8fed211947ecb62585
NicholasC2 <nicholasc2@proton.me>
2026-02-12 12:19:02 +1300

    did stuf

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-import { createAccount, updateAccount } from "./account.ts"
+import { createAccount } from "./account.js"
+import Express from "express"
+import cors from "cors"
+
+const app = Express();
+
+app.use(cors({
+    origin: "*"
+}))
+app.use(Express.json());
+app.use(Express.static("public/"));
+
+app.post("/account/:action", (req, res) => {
+    switch(req.params.action) {
+        case "create": {
+            res.json(req.body)
+            
+            break;
+        }
+    }
+});
+
+app.listen(4000, function() {
+    console.log("Server Listening on port 4000")
+})
