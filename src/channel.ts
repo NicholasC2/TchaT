002941a6a77639a8356197767259d1f75573790b
NicholasC2 <nicholasc2@proton.me>
2026-03-30 14:14:05 +1300

    moved message class to channel

--- src/channel.ts
+++ src/channel.ts
@@ -1,5 +1,28 @@
-import { getAccountByUsername } from "./account.service.js";
-import { Message } from "./message.service.js";
+import { getAccountByUsername, Account } from "./account.service.js";
+
+export class Message {
+    content: string;
+    author: Account;
+
+    constructor(content: string, author: Account) {
+        this.content = content;
+        this.author = author;
+    }
+
+    serialize() {
+        return {
+            content: this.content,
+            author: this.author.username
+        };
+    }
+
+    static deserialize(data: { content: string, author: string }, getAccountByUsername: (username: string) => Account | null) {
+        const author = getAccountByUsername(data.author);
+        if (!author) throw new Error("Author account not found");
+        return new Message(data.content, author);
+    }
+}
+
 
 export class Channel {
     name: string;
