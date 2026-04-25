a2a7a1a9eba1baa2ff87da097172aa090c75a339
NicholasC2 <nicholasc2@proton.me>
2026-04-25 15:53:03 +1200

    added heartbeat

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Account, Session } from "ts-accountd"
 import { Config, defaultConfig } from "./config.js"
 import { WebSocketServer } from "ws"
 
+import { Message_Type, Socket_Close_Reason } from "./message_helper.js"
+
 console.log("Starting TchaT server...")
 
 let config = defaultConfig;
@@ -18,4 +20,27 @@ const server = new WebSocketServer({
 
 server.on("listening", ()=>{
     console.log(`Server listening on port ${config.getPort()}`)
+})
+
+server.on("connection", (socket)=>{
+    let heartbeatTime = Date.now()
+
+    socket.on("message", (rawMessage)=>{
+        const parsed = JSON.parse(rawMessage.toString());
+
+        const type = parsed.t;
+        const data = parsed.d;
+
+        if(type !instanceof Number) throw new Error();
+
+        if(type == Message_Type.HEARTBEAT) {
+            heartbeatTime = Date.now();
+        }
+    })
+
+    setInterval(()=>{
+        if(heartbeatTime + 30000 < Date.now()) {
+            socket.close(Socket_Close_Reason.HEARTBEAT_TIMEOUT);
+        }
+    }, 5000)
 })
---
+++ src/message_helper.ts
@@ -0,0 +1,8 @@
+export enum Message_Type {
+    HEARTBEAT
+}
+
+export enum Socket_Close_Reason {
+    UNKNOWN,
+    HEARTBEAT_TIMEOUT
+}
